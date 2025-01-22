89ab8f5c614844452e85173a7dceacb122da6c5b
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2025-01-22 19:15:06 +1000

    Added common denominator and mixed fraction support

--- test/check.f90
+++ test/check.f90
@@ -35,9 +35,9 @@ program check
     call assert_equals(fr%denominator,12,0)
 
     fr=add_fraction(1, i, 2, i)
-    call test("1/MAXINT + 2/MAXIT = 3/MAXINT")
+    call test("1/MAXINT + 2/MAXINT = 3/MAXINT")
     call assert_equals(fr%numerator,3,0)
-    call test("1/MAXINT + 2/MAXIT = 3/MAXINT")
+    call test("1/MAXINT + 2/MAXINT = 3/MAXINT")
     call assert_equals(fr%denominator,i,0)
 
     ! **** Subtraction **** !
@@ -55,29 +55,29 @@ program check
     call assert_equals(fr%denominator,12,0)
     
     fr=sub_fraction(2, i, 1, i)
-    call test("2/MAXINT - 1/MAXIT = 1/MAXINT")
+    call test("2/MAXINT - 1/MAXINT = 1/MAXINT")
     call assert_equals(fr%numerator,1,0)
-    call test("2/MAXINT - 1/MAXIT = 3/MAXINT")
+    call test("2/MAXINT - 1/MAXINT = 3/MAXINT")
     call assert_equals(fr%denominator,i,0)
 
     ! **** Multiply Fraction **** !
 
     fr = multiply_fraction(f1,f2)
-    call test("1/2 + 1/3 = 1/6 Numerator = 1")
+    call test("1/2 * 1/3 = 1/6 Numerator = 1")
     call assert_equals(fr%numerator,1,0)
-    call test("1/2 + 1/3 = 1/6 Denominator = 6")
+    call test("1/2 * 1/3 = 1/6 Denominator = 6")
     call assert_equals(fr%denominator,6,0)
 
     fr = multiply_fraction(1, 3, 1, 4)
-    call test("1/3 + 1/4 = 1/12 Numerator = 1")
+    call test("1/3 * 1/4 = 1/12 Numerator = 1")
     call assert_equals(fr%numerator,1,0)
-    call test("1/3 + 1/4 = 1/12 Denominator = 12")
+    call test("1/3 * 1/4 = 1/12 Denominator = 12")
     call assert_equals(fr%denominator,12,0)
 
     fr=multiply_fraction(1, i, 2, i)
-    call test("1/MAXINT + 2/MAXIT = 2/(MAXINT*MAXINT) AKA Integer Overflow")
+    call test("1/MAXINT * 2/MAXINT = 2/(MAXINT*MAXINT) AKA Integer Overflow")
     call assert_equals(fr%status,'Error: Integer Overflow')
-    call test("1/MAXINT + 2/MAXIT = 2/(MAXINT*MAXINT) AKA Integer Overflow")
+    call test("1/MAXINT * 2/MAXINT = 2/(MAXINT*MAXINT) AKA Integer Overflow")
     call assert_equals(fr%status,'Error: Integer Overflow')
 
      ! **** Divide Fraction **** !
@@ -95,10 +95,32 @@ program check
     call assert_equals(fr%denominator,3,0)
 
     fr=divide_fraction(2, i, 1, i)
-    call test("2/MAXINT / 1/MAXIT = 2*MAXINT/MAXINT")
+    call test("2/MAXINT / 1/MAXINT = 2*MAXINT/MAXINT")
     call assert_equals(fr%status,'Error: Integer Overflow')
-    call test("2/MAXINT - 1/MAXIT = 2*MAXINT/MAXINT")
+    call test("2/MAXINT / 1/MAXINT = 2*MAXINT/MAXINT")
     call assert_equals(fr%status,'Error: Integer Overflow')
+
+    ! **** Lowest Common Denominator *** !
+    
+    fr = lowest_common_denom(8, 16)
+    call test("LCD: 8/16 = 1/2")
+    call assert_equals(fr%numerator, 1, 0)
+    call test("LCD: 8/16 = 1/2")
+    call assert_equals(fr%denominator, 2, 0)
+
+    fr = lowest_common_denom(9, 27)
+    call test("LCD: 9/27 = 1/3")
+    call assert_equals(fr%numerator, 1, 0)
+    call test("LCD: 9/27 = 1/3")
+    call assert_equals(fr%denominator, 3, 0)
+
+    fr = lowest_common_denom(3, 2)
+    call test("LCD: 3/2 = 1 1/2")
+    call assert_equals(fr%unit, 1, 0)
+    call test("LCD: 3/2 = 1 1/2")
+    call assert_equals(fr%numerator, 1, 0)
+    call test("LCD: 3/2 = 1 1/2")
+    call assert_equals(fr%denominator, 2, 0)
 
     call results()
 
